2cd89ce5bc86f5e97df7b7cb22f3ad656e63279d
something252 <thetakeall21@gmail.com>
2014-03-20 17:20:58 -0400

    initial commit

---
+++ src/com/blank/develop/multicalculator/VarJar.java
@@ -0,0 +1,15 @@
+package com.blank.develop.multicalculator;
+
+
+
+// variables class
+public final class VarJar{
+		
+	private VarJar(){}
+	
+	public static int PL_Count=0; // Parenthesis left counter
+	public static int PR_Count=0; // Parenthesis right counter
+	public static int count=0; // debug related
+	
+	
+}
